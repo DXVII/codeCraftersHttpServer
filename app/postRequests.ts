ca77a785ec55a1f0e0904c464c84077dfea26f05
David <d.pham@student.tudelft.nl>
2024-07-17 20:28:48 +1000

    read request body

--- app/postRequests.ts
+++ app/postRequests.ts
@@ -4,16 +4,26 @@ import { writeFileSync } from "fs";
 export const handlePostRequests = (socket:Socket, request:string) => {
      const path = request.split(' ')[1];
 
-     if(path.includes("/files/")) postPathFiles(socket,path);
+
+     if(path.includes("/files/")) postPathFiles(socket,request);
      else writeSocketError
 
 
 }
 
-const postPathFiles = (socket:Socket,path:string) => {
-     console.log(process.argv);
+const postPathFiles = (socket:Socket,request:string) => {
      const dir = process.argv[3];
-     // const fileName = path.split("/files/")[1];
-     // writeFileSync(dir+fileName,socket.read());
-     writeSocketSuccess201(socket,"text/plain","");
+     
+     const path = request.split(' ')[1];
+     const fileName = path.split("/files/")[1];
+     const requestLineSplit = request.split('\r\n');
+     const contentBody = requestLineSplit[requestLineSplit.length-1];
+
+     try {
+          writeFileSync(dir+fileName,contentBody);
+          writeSocketSuccess201(socket,"text/plain","");
+     } catch {
+          writeSocketError(socket)
+     }
+     
 }
