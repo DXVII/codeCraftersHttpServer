51d5b32d46c8232753678e8be566b1b815dadf46
DXVII <david.pham.d@gmail.com>
2024-07-17 17:53:10 +1000

    testing server

--- app/main.ts
+++ app/main.ts
@@ -1,22 +1,35 @@
-import express from "express";
+import * as net from 'net';
 
-const app = express()
-
-app.get('/', (req, res) => {
-  res.status(200).send("OK")
+const server = net.createServer(
+  (socket) => {
+    socket.on('data',(data)=>{
+        const request = data.toString();
+        console.log(request);
+        // const path = request.split(' ')[1];
+        // const response = path === '/' ? 'HTTP/1.1 200 OK\r\n\r\n' : 'HTTP/1.1 404 Not Found\r\n\r\n';
+        socket.write("200 OK");
+        socket.end();
+    })
 });
-
-app.use(function(req, res, next) {
-  res.status(404).send('Not Found\r\n\r\n');
-});
-
-app.listen(4221,() => console.log("Listening on http://localhost:4221"));
-
-// import * as net from "net";
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
-// const server = net.createServer((socket) => {
-//   socket.write("HTTP/1.1 200 OK\r\n\r\n")
-//   socket.on("close", () => {
-//     socket.end();
-//   });
+server.listen(4221, 'localhost', () => {
+    console.log('Server is running on port 4221');
+});
+
+
+// import express from "express";
+
+// const app = express()
+
+// app.get('/', (req, res) => {
+//   res.status(200).send("OK")
 // });
+
+// app.use(function(req, res, next) {
+//   res.status(404).send('Not Found\r\n\r\n');
+// });
+
+// app.listen(4221,() => console.log("Listening on http://localhost:4221"));
+
