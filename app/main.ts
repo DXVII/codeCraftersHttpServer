230cfebf77af4dca4653a4ff48058862f024f1a5
David <d.pham@student.tudelft.nl>
2024-07-17 19:46:33 +1000

    return a file

--- app/main.ts
+++ app/main.ts
@@ -1,35 +1,39 @@
 import {Socket, createServer} from 'net';
-
-const responseFormat = (status: string,suffix:string) => {
-  const statusStr = `HTTP/1.1 ${status}\r\n`
-  const contentType = `Content-Type: text/plain\r\n`
-  const contentLength = `Content-Length: ${suffix.length}\r\n`
-  return statusStr+contentType+contentLength+"\r\n"+suffix
-};
+import { readFileSync } from 'fs';
 
 const server = createServer((socket: Socket) => {
     socket.on('data',(data:Buffer) => {
       const request = data.toString()
       const path = request.split(' ')[1];
 
-
       if(path === "/"){
-        socket.write(responseFormat("200 OK",""));
+        writeSocketSuccess(socket,"text/plain","");
       }
 
       else if(path.includes("/echo/")){
         const suffix = path.split("/echo/")[1];
-        socket.write(responseFormat("200 OK",suffix));
+        writeSocketSuccess(socket,"text/plain",suffix);
         
       } 
       
       else if(path.includes("/user-agent")){
         const userAgentContent = (request.split("User-Agent: ")[1]).replace("\r\n\r\n","");
-        socket.write(responseFormat("200 OK",userAgentContent));
+        writeSocketSuccess(socket,"text/plain",userAgentContent);
       }
-      
+
+      else if(path.includes("/files/")){
+        // console.log(process.argv); ==> [ "/usr/local/bin/bun", "/app/app/main.ts", "--directory", "/tmp/data/codecrafters.io/http-server-tester/"
+        const dir = process.argv[3];
+        const fileName = path.split("/files/")[1];
+        try{
+          const filecontent = readFileSync(dir+fileName).toString();
+          writeSocketSuccess(socket,"application/octet-stream",filecontent);
+        } catch{
+          writeSocketError(socket);
+        }
+      }      
       else {
-        socket.write(responseFormat("404 Not Found",""));
+        writeSocketError(socket);
       }
       socket.end();
   })
@@ -38,6 +42,19 @@ const server = createServer((socket: Socket) => {
 server.listen(4221, 'localhost', 
   () => console.log('Server is running on port 4221')
 );
+
+
+const responseFormat = (status: string,contentType:string,suffix:string) => {
+  const statusStr = `HTTP/1.1 ${status}\r\n`
+  const contentTypeStr = `Content-Type: ${contentType}\r\n`
+  const contentLength = `Content-Length: ${suffix.length}\r\n`
+  return statusStr+contentTypeStr+contentLength+"\r\n"+suffix
+};
+
+
+const writeSocketError = (socket:Socket) =>socket.write(responseFormat("404 Not Found","text/plain",""));
+const writeSocketSuccess = (socket:Socket,contentType:string,content:string) => socket.write(responseFormat("200 OK",contentType,content));
+
 
 
 // import express from "express";
